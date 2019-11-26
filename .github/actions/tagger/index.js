4111be500cee40a2315eef3add9459843f6af082
Jason Quense <monastic.panic@gmail.com>
2019-11-26 14:01:28 -0500

    add action instead

---
+++ .github/actions/tagger/index.js
@@ -0,0 +1,48 @@
+const core = require("@actions/core");
+const path = require("path");
+const github = require("@actions/github");
+const execa = require("execa");
+
+async function run() {
+  try {
+    console.log(path.resolve(__dirname, "../../../"));
+    const resp = await execa("lerna", ["changed", "--json"], {
+      cwd: path.resolve(__dirname, "../../../")
+    });
+
+    const changed = JSON.parse(resp || "[]");
+
+    const token = core.getInput("token", { required: true });
+    const sha = github.context.sha;
+
+    const client = new github.GitHub(token);
+
+    core.debug(`tagging #${sha} with tag ${tag}`);
+
+    const tags = changed.map(({ name, version }) => `${name}@${tag}`);
+
+    await Promise.all(
+      tags.flatMap(tag => [
+        client.git.createRef({
+          owner: github.context.repo.owner,
+          repo: github.context.repo.repo,
+          ref: `refs/tags/${tag}`,
+          sha: sha
+        }),
+        client.git.createTag({
+          tag,
+          message: tag,
+          type: "commit",
+          object: sha,
+          owner: github.context.repo.owner,
+          repo: github.context.repo.repo
+        })
+      ])
+    );
+  } catch (error) {
+    core.error(error);
+    core.setFailed(error.message);
+  }
+}
+
+run();
